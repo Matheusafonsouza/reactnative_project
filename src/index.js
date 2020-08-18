3edcb3bae55c608148da8ab025c44638c173b295
Matheus Afonso <matheusafonsouza@gmail.com>
2020-08-18 19:54:16 -0300

    feature: Created projects list and integrated api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar } from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        api.get('projects').then(response => {
+            setProjects(response.data);
+        });
+    }, []);
+
     return (
-        <View />
+        <>
+            <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+            
+            <SafeAreaView style={styles.container}>
+                <FlatList 
+                    data={projects}
+                    keyExtractor={project => project.id}
+                    renderItem={({ item: project }) => (
+                        <Text key={project.id} style={styles.project}>{project.title}</Text>
+                    )}
+                />
+            </SafeAreaView>
+        </>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+    },
+
+    project: {
+        color: '#fff',
+        fontSize: 30
+    }
+});
